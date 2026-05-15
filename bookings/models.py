aeedaca3866647972b7366992f6a5ef4feb57fe8
Gyatari-Bhandare-2002 <gayatribhandare2002@gmail.com>
2026-05-15 13:47:07 +0530

    revert: remove user login system

--- bookings/models.py
+++ bookings/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User 
+
 from django.utils import timezone
 
 DAY_TYPE_CHOICES = [
@@ -73,7 +73,7 @@ def get_fee(day_type, slot):
 
 
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True) 
+
     name          = models.CharField(max_length=100)
     email         = models.EmailField()
     phone         = models.CharField(max_length=15)
